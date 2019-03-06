ebb772c57498df70e67181bb464d0ea86601130a
Adele Ardent <47130360+AdeleArdent@users.noreply.github.com>
2019-03-06 13:01:59 -0700

    Create 2019-2-27-cripps.m

---
+++ _posts/2019-2-27-cripps.m
@@ -0,0 +1,35 @@
+---
+layout: post
+title: A Visit to the Cripps Lab
+---
+
+# Students Use CRISPR to Modify Flies
+
+A few weeks ago, I visted the former Cripps lab. The principal investigator, Richard Cripps, recently moved his lab to San Diego State University, and the research at UNM is now being directed by Assistant Professor TyAnna Lovato. This group is interested in learning more about muscle development and function using fruit flies as a model organism. On the lab’s website, they mention that over 75% of the human disease gene entries found in OMIM have high similarity to genes found in fruit flies.
+
+The former Cripps lab also oversees the BIO 498 Genome Editing class, wherein students can use CRISPR techniques. CRISPR stands for Clustered Regularly Interspaced Short Palindromic Repeats. CRISPR was discovered in 2007 when another group of researchers noticed short repeating sections in the genome in Streptococcus thermophilus, which is a bacteria used in cheese and yogurt making. These sections were palindromes sandwiched between more randomized sections that first appeared to be junk. But as the repeats were highly structured, this was an indication that these stretch of the genome was important for something.
+
+Eventually, it was revealed that the seemingly random sections between the palindromic sections are actually pieces of DNA from bacteriophages, which are viruses that infect bacteria. If a bacteria survives an encounter with a bacteriophage, it wants to be able recognize that it is being attacked much more quickly in the future, so it can fight it off before the bacteriophage gets a foothold inside the cell. As a defense, it chops up the viral genome, which prevents the virus from reproducing, but it saves some of that chopped-up DNA so that it can use that chunk as a template to compare to intrusive DNA in the future, creating a sort of genomic mug-shot. (The palindromic sequences are basically there as spacers.) CRISPR has since been adapted to help target specific cuts in the genome in laboratory settings.
+
+The Cripps group used this technology to help students create null mutations in genes that were uncharacterized, but which were suspected to have some role in muscle development or function. (A null mutation is a mutation that knocks out the creation of a protein or gene product, rather than introducing something like a slight structural change to a protein.) 
+
+The lab identifies genes that they want to look at based on their own or others previous speculation that they might do something interesting in muscle. They then pull the sequence from FlyBase; as a well characterized model organism, the entire genome of Drosophila has been sequenced and much of it has been characterized.
+
+Then, the researchers design a guide sequence to aim CRISPR’s cutting ability at that particular region to prepare the fly’s DNA for insertion of the sequence they want to add. They use the CRISPR targeting site to check that they won’t make cuts elsewhere in the genome, and adjust their guide sequence, the targeting system, as needed. 
+
+Drosophila eggs/embryos are visible to the naked eye. To collect them, the lab uses agar plates that have grape juice added. They also smear yeast paste on it as well as a food source for the adults. The plates are attached to the end of a large vial, and the grape juice attracts the females to lay eggs on it, since fruit flies lay eggs on rotting fruit (as those of us who are indifferent housekeepers know well.)
+
+![TyAnna Lovato](/assets/images/20190227_flyjar.jpg)
+
+The embryos are then collected, and a super thin glass needle attached to a microscope is used to inject a dose of the Cas-9 mixture into the posterior end of the embryos. First though, the embryos are soaked in a 50% bleach solution to remove a tough outer membrane, and they have to be put briefly in a desiccating chamber to dehydrate them a little so the injection of additional fluid doesn’t cause them to pop like a water balloon. Then you grow the flies and look for those that have the genetic modification you desire.
+
+![TyAnna Lovato](/assets/images/20190227_lovato.jpg)
+
+A few notes about safety: In addition to just general sterile technique and chemical safety, the lab has to exercise a little extra caution, since they are working with genetically modified organisms. They have warnings posted outside the lab, but you can also see that it is only a level 1 biosafety concern. 
+
+![Biosafety warming.](/assets/images/20190227_biohazl.jpg)
+
+Therefore, before disposing of the flies and their housing in the trash, they freeze them for 24 hours, just to make sure that they are actually dead. They also have this oil traps scattered around the lab to trap any escapees. But, you can see these are pretty minimal precautions that are more related to the fact that flies are small and mobile, rather than any large risk to humanity.
+
+![Oil for trapping stray flies.](/assets/images/20190227_flyoil.jpg)
+
